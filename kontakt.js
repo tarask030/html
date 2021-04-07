7aff37c4251e6984e2a621f26984f859d0ac974d
tarask030 <38398391+tarask030@users.noreply.github.com>
2021-04-07 15:04:01 +0200

    Add files via upload

---
+++ kontakt.js
@@ -0,0 +1,14 @@
+function showDiv() {
+  document.getElementById('welcomeDiv').style.display = "block";
+}
+
+function hideDiv(){
+  document.getElementById('welcomeDiv').style.display = "none";
+}
+
+function showDivGratulation() {
+  document.getElementById('welcomeDivGratulation').style.display = "block";
+}
+function hideDivGratulation(){
+  document.getElementById('welcomeDivGratulation').style.display = "none";
+}
